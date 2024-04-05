6d152cc0406ed40188fd10e8e9b57756d11dd38a
Jomiel <enriquez.jliquigan@gmail.com>
2024-04-05 13:26:28 +0800

    Add project files.

---
+++ dataapi/Model/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿namespace dataapi.Model
+{
+    using Microsoft.EntityFrameworkCore;
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<TBL_DISPENSELOG> TBL_DISPENSELOG { get; set; }
+    }
+}
---
+++ dataapi/Model/TBL_DISPENSELOG.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace dataapi.Model
+{
+    public class TBL_DISPENSELOG
+    {
+        [Key]
+        [Required]
+        public Guid DISPENSELOGID { get; set; }
+        public Decimal Amount { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+    }
+}
