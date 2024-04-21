76dd5cc74db7903ec16186eba8cdcfa5d7d9589e
Jomiel <enriquez.jliquigan@gmail.com>
2024-04-21 19:16:44 +0800

    Adding Time

---
+++ dataapi/Controllers/TimeController.cs
@@ -0,0 +1,19 @@
+﻿using dataapi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace dataapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<DateTime> Get()
+        {
+            return DateTime.Now;
+        }
+    }
+}
